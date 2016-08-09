78a38ec7f6afe2928309e451f89c24c5dfddd36c
BroVic <victorordu@outlook.com>
2016-08-09 17:23:43 +0100

    Applies reactive expression.
    Tweets are loaded directly into this code and built into a reactive expresssion. As tweets are loaded live (for now) the instruction to read data from file is removed.

--- shinyNESREA/Server.R
+++ shinyNESREA/Server.R
@@ -2,14 +2,20 @@
 # A Shiny App for Exploratory Data Analysis of the NESREA Twitter handle
 
 library(shiny)
+library(twitteR)
 library(ggplot2); theme_set(new = theme_bw())
-
-df <- readRDS("~/7-NESREA/SA/WMG/socialmedia/shinyNESREA/data/nesrea-tweet-df.rds")
 
 shinyServer(function(input, output) {
   
+  dataInput <- reactive({
+    tweets <- searchTwitter("NESREA", n = 100,
+                            since = as.character(input$startDate),
+                            until = as.character(input$endDate))
+    df <- twListToDF(tweets)
+  })
+  
   output$twtDensity <- renderPlot({
-    tweetDistr <- ggplot(df, aes(created)) +
+    tweetDistr <- ggplot(dataInput(), aes(created)) +
       geom_density(aes(fill = isRetweet), alpha = .5) +
       theme(legend.justification = c(1, 1), legend.position = c(1, 1)) +
       xlab("All tweets")
