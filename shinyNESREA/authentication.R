3b4a6c378440932bd95bbb11e1a5e84d01c3103b
BroVic <victorordu@outlook.com>
2016-08-22 17:00:24 +0100

    Updated path of file with OAuth credentials

--- shinyNESREA/authentication.R
+++ shinyNESREA/authentication.R
@@ -3,6 +3,6 @@
 
 # To download data
 library(twitteR)
-source("~/7-NESREA/SA/WMG/socialmedia/nesrea_twitterAuth.R")
+source("~/7-NESREA/SA/WMG/NESREA_social/nesrea_twitterAuth.R")
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 
