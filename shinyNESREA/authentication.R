29f5571fb72064514346165e208cea99fefff435
BroVic <victorordu@outlook.com>
2016-09-09 16:09:12 +0100

    Use Twitter OAuth credentials directly
    Twitter OAuth credentials are added directly to this script rather than sourcing them from a separate file outside the Shiny app.

--- shinyNESREA/authentication.R
+++ shinyNESREA/authentication.R
@@ -3,6 +3,12 @@
 
 # To download data
 library(twitteR)
-source("~/7-NESREA/SA/WMG/NESREA_social/nesrea_twitterAuth.R")
+library(httr)
+
+consumer_key <- "x7TKjWEt0jYhgnZptAt9COKRL"
+consumer_secret <- "q5LWFSHu2of2and7a1vl0dLGpez03bU5qvXpkbX2NklZJt4f4r"
+access_token <- "742691184267677696-2HZMvozNMdt5qIqZjR9FLZJ0ctDUiEs"
+access_secret <- "qB58UlnMDQfpqAhDHHvTpxEVwmANzJXfowb6lJNm7Cyyl"
+
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 
