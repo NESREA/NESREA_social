b33eec34594148d5d145e9830872779b132b2e33
BroVic <victorordu@outlook.com>
2016-11-16 18:29:12 +0100

    Actual initial commit

---
+++ facebook-general.R
@@ -0,0 +1,9 @@
+NESREA_page_id <- "145175075891647"
+
+NESREA_token <- "EAAC5BzMc3YEBAHy7yI3iDzurRbBWNAkkTIp7YKZBZCepp08Ahz6U0rmzlFhZBKXWmbPvZBi7pGIMvy7BWcUa45qwEiCU0aQZAUTzsTO0PdgddguYyvpaaehOIZACRDBXNp03QFZCmJMlNjZA7z9FUpeH2DwjiILZB6IL97yNjWiWkHMqaHvfA6ufoTJJSrZCe0JGwZD"
+
+API_version <- "2.8"
+
+insights <- getInsights(object_id = NESREA_page_id, token = NESREA_token,
+            metric = "page_impressions")
+
