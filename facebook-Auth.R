e0e3153f48283f987021cfbbde69613d864eed7d
BroVic <victorordu@outlook.com>
2016-11-16 17:30:13 +0100

    Clear workspace

--- facebook-Auth.R
+++ facebook-Auth.R
@@ -5,6 +5,8 @@ app_id <- "203440573439361"
 app_secret <- "c3b0eecbe6bfeb465438a68e8205c67b"
 
 NESREA_fb_oauth <- Rfacebook::fbOAuth(app_id = app_id, app_secret = app_secret)
-save(NESREA_fb_oauth, file = "NESREA_fb_oauth")
+saveRDS(NESREA_fb_oauth, file = "NESREA_fb_oauth.rds")
 
 # To use, run: load("NESREA_fb_oauth")
+
+rm(list = ls())
