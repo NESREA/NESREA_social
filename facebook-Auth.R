cd39299144f9a75eb62e261173fd160d247d9ca3
BroVic <victorordu@outlook.com>
2016-11-16 17:05:18 +0100

    Save token
    The returned value of fbOAuth() is now saved in the working directory.

--- facebook-Auth.R
+++ facebook-Auth.R
@@ -1,5 +1,10 @@
+# facebook-Auth.R
 # API access for the 'NESREA' Facebook app
-app_id <- 203440573439361
+
+app_id <- "203440573439361"
 app_secret <- "c3b0eecbe6bfeb465438a68e8205c67b"
 
-Rfacebook::fbOAuth(app_id = app_id, app_secret = app_secret)
+NESREA_fb_oauth <- Rfacebook::fbOAuth(app_id = app_id, app_secret = app_secret)
+save(NESREA_fb_oauth, file = "NESREA_fb_oauth")
+
+# To use, run: load("NESREA_fb_oauth")
