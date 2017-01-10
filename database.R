13fd2e8e27ac6d21287d096416cdd18cd4807c43
BroVic <victorordu@outlook.com>
2017-01-10 18:07:42 +0100

    Initial commit.

---
+++ database.R
@@ -0,0 +1,21 @@
+# database.R
+# Analyses on stored social media data
+
+library(twitteR)
+library(ggplot2)
+
+tables <- 
+  dbListTables(register_sqlite_backend("shinyNESREA/data/nesreanigeria.db"))
+tables
+
+tweetdata <- load_tweets_db(as.data.frame = TRUE,
+                            table_name = tables[1])
+
+# Exploratory analysis
+str(tweetdata)
+colnames(tweetdata)
+adjust_tweetdata <- 
+  dplyr::select(tweetdata, c(text:created, replyToUID:retweeted))
+summary(adjust_tweetdata)
+number_of_tweets <- nrow(tweetdata)
+format(range(tweetdata$created), "%d %b %m %Y")
