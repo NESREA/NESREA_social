16808c4f42be9fe8325afeb301377846d8778780
Remi-Agunbiade <remy.agunbiade@gmail.com>
2017-04-05 16:33:23 +0100

    Add files via upload

---
+++ facebook-Auth.R
@@ -0,0 +1,12 @@
+# facebook-Auth.R
+# API access for the 'NESREA' Facebook app
+
+app_id <- "203440573439361"
+app_secret <- "c3b0eecbe6bfeb465438a68e8205c67b"
+
+NESREA_fb_oauth <- Rfacebook::fbOAuth(app_id = app_id, app_secret = app_secret)
+saveRDS(NESREA_fb_oauth, file = "NESREA_fb_oauth.rds")
+
+# To use, run: readRDS("NESREA_fb_oauth.rds")
+
+rm(list = ls())
