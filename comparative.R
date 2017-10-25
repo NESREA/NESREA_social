ba0d9131c3009c1e4daaadb066958cb3614df51a
Victor Ordu <victorordu@outlook.com>
2017-10-25 17:56:58 +0100

    Uses more descriptive variable names

--- comparative.R
+++ comparative.R
@@ -4,6 +4,8 @@
                  #          PRELIMINARIES/SETUP               #
                  ##############################################
 library(twitteR)
+library(magrittr)
+                 
 source("twitter-functions.R")
 check_wd()
 logon_to_twitter()
@@ -13,19 +15,23 @@ logon_to_twitter()
                 #             ANALYSIS PROPER                #
                 ##############################################
 # Start with the small fry
-mentions <- c("@nesreanigeria", "@nosdra", "@biosafetyng")
-result <- compare_mentions(mentions)
-result
+handles <- c("@nesreanigeria", "@nosdra", "@biosafetyng")
+result <- compare_mentions(handles)
+# result <- result %>%
+#   sapply(length) %>%
+#   as.table(.)
 
 chart(result)
 
 # Add FMEnv
-mentions <- c(mentions, "@FMENVNg")
-result2 <- compare_mentions(mentions)
-result2
+handles <- c(handles, "@FMENVNg")
+result2 <- compare_mentions(handles)
+# result2 <- result2 %>%
+#   sapply(length) %>%
+#   as.table(.)
 
 chart(result2)
 
 # Optionally increase the download limit, where one of the terms hits maximum
-chart(compare_mentions(mentions, n = 200))
-chart(compare_mentions(mentions, n = 500))
+chart(compare_mentions(handles, n = 200))
+chart(compare_mentions(handles, n = 500))
