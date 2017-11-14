ba74ce62f884a1cc9381023f3f6c518c3d05d880
BroVic <victorordu@outlook.com>
2017-11-14 18:21:07 +0100

    (Finally) brings together FB data operations

--- facebook/facebook-general.R
+++ facebook/facebook-general.R
@@ -1,4 +1,4 @@
-# facebook-general.R
+## facebook-general.R
 
 library(Rfacebook)
 library(dplyr)
@@ -15,7 +15,7 @@ chooseInsight <- function(type = c("page_fan_adds",
                                    "page_views",
                                    "page_story_adds",
                                    "page_impressions",
-                                   "page_posts_imporessions",
+                                   "page_posts_impressions",
                                    "page_consumptions",
                                    "post_consumptions_by_type",
                                    "page_fans_country")) {
@@ -23,38 +23,34 @@ chooseInsight <- function(type = c("page_fan_adds",
   result <- getInsights(object_id = NESREA_page_id,
                         token = nesreaToken,
                         metric = type,
-                        version = API_version)
-  result <- select(result, value:end_time)
+                        version = API_version) %>%
+    select(value:end_time)
   result$end_time <- substr(result$end_time,
                             start = 1,
                             stop = regexpr("T", result$end_time) - 1)
   result 
 }
 
-newFans <- chooseInsight("page_fan_adds")
-fansLeft <- chooseInsight("page_fan_removes")
-pageViewsLogin <- chooseInsight("page_views_login")
-pageViews <- chooseInsight("page_views")
-newStories <- chooseInsight("page_story_adds")
-pageImpressions <- chooseInsight("page_impressions")
-postImpressions <- chooseInsight("page_posts_impressions")
-pageConsumptions <- chooseInsight("page_consumptions")
+insight.type <- c("page_fan_adds", "page_fan_removes", "page_views_login",
+                  "page_views", "page_story_adds", "page_impressions",
+                  "page_posts_impressions", "page_consumptions")
 
-## build a single dataframe from these objects
-allInsights <- data.frame(newFans[, 1], fansLeft[, 1], pageViewsLogin[, 1],
-                          pageViews[, 1], newStories[, 1], pageImpressions[, 1],
-                          postImpressions[, 1], pageConsumptions[, 1],
-                          endtime = pageConsumptions[, 2])
-colnames(allInsights) <- gsub("[^[:alpha:]]", "", colnames(allInsights))
-summary(allInsights)
+allInsights <- as.data.frame(lapply(insight.type, chooseInsight)) %>%
+  select(matches("value|end_time.7")) %>%
+  distinct(.)
+
+colnames(allInsights)[1:8] <- gsub("^page_", "", insight.type)
+colnames(allInsights)[9] <- "date"
+allInsights$date <- as.Date(allInsights$date)
+str(allInsights)
 
 ## PUblic posts
-page_posts <- getPage(page = "nesreanigeria",
+page_posts <- getPage(page = "nesreanigeria", n = 100,
                       token = nesreaToken, feed = TRUE) %>%
-  select(., c(message:type, likes_count:shares_count))
+  select(c(message:type, likes_count:shares_count))
 colnames(page_posts) <- gsub("_count$|_time$", "", colnames(page_posts))
 page_posts$message <- page_posts$message %>%
-  gsub("[[^:graph:]]", "", .) %>%
+  gsub("[^[:graph:]]", " ", .) %>%
   str_trim(.)
 page_posts$type <- as.factor(page_posts$type)
 page_posts$created <- substr(page_posts$created, start = 1,
