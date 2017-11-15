034a67bcbb052b220314def3d5f5e55db14c684d
Victor Ordu <victorordu@outlook.com>
2017-11-15 09:22:44 +0100

    Renames the file

---
+++ facebook/facebook.R
@@ -0,0 +1,38 @@
+## facebook-general.R
+
+library(Rfacebook)
+library(dplyr)
+library(stringr)
+
+sapply(c("fb-functions.R", "facebook/fb_auth.R"), source)
+
+## Insights on NESREA Page
+insight.type <- c("page_fan_adds", "page_fan_removes", "page_views_login",
+                  "page_views", "page_story_adds", "page_impressions",
+                  "page_posts_impressions", "page_consumptions")
+
+allInsights <- as.data.frame(lapply(insight.type, chooseInsight)) %>%
+  select(matches("value|end_time.7")) %>%
+  distinct(.)
+
+colnames(allInsights)[1:8] <- gsub("^page_", "", insight.type)
+colnames(allInsights)[9] <- "date"
+allInsights$date <- as.Date(allInsights$date)
+str(allInsights)
+
+## PUblic posts
+page_posts <- getPage(page = "nesreanigeria", n = 100,
+                      token = nesreaToken, feed = TRUE) %>%
+  select(c(message:type, likes_count:shares_count))
+
+colnames(page_posts) <- gsub("_count$|_time$", "", colnames(page_posts))
+
+page_posts$message <- page_posts$message %>%
+  gsub("[^[:graph:]]", " ", .) %>%
+  str_trim(.)
+
+page_posts$type <- as.factor(page_posts$type)
+
+page_posts$created <- substr(page_posts$created, start = 1,
+                        stop = regexpr("T", page_posts$created) - 1)
+page_posts
