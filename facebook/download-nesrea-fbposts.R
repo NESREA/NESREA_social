e1cf3e74ff016c711527636e8b67e6d441a77277
Victor Ordu <victorordu@outlook.com>
2017-11-16 17:50:06 +0100

    Reformats code for better readability
    Done with IDE tool.

--- facebook/download-nesrea-fbposts.R
+++ facebook/download-nesrea-fbposts.R
@@ -1,19 +1,29 @@
 ## download-nesrea-fbposts.R
-
+library(DBI)
+library(RSQLite)
 library(Rfacebook)
 library(dplyr)
 library(stringr)
 
-invisible(
-  sapply(c("facebook/fb-functions.R", "facebook/fb_auth.R"), source)
-)
+invisible(sapply(c(
+  "facebook/fb-functions.R", "facebook/fb_auth.R"
+), source))
 
 ## Insights on NESREA Page
-insight.type <- c("page_fan_adds", "page_fan_removes", "page_views_login",
-                  "page_views", "page_story_adds", "page_impressions",
-                  "page_posts_impressions", "page_consumptions")
+insight.type <-
+  c(
+    "page_fan_adds",
+    "page_fan_removes",
+    "page_views_login",
+    "page_views",
+    "page_story_adds",
+    "page_impressions",
+    "page_posts_impressions",
+    "page_consumptions"
+  )
 
-allInsights <- as.data.frame(lapply(insight.type, chooseInsight)) %>%
+allInsights <-
+  as.data.frame(lapply(insight.type, chooseInsight)) %>%
   select(matches("value|end_time.7")) %>%
   distinct(.)
 
@@ -23,11 +33,16 @@ allInsights$date <- as.Date(allInsights$date)
 str(allInsights)
 
 ## PUblic posts
-page_posts <- getPage(page = "nesreanigeria", n = 100,
-                      token = nesreaToken, feed = TRUE) %>%
+page_posts <- getPage(
+  page = "nesreanigeria",
+  n = 100,
+  token = nesreaToken,
+  feed = TRUE
+) %>%
   select(c(message:shares_count))
 
-colnames(page_posts) <- gsub("_count$|_time$", "", colnames(page_posts))
+colnames(page_posts) <-
+  gsub("_count$|_time$", "", colnames(page_posts))
 
 page_posts$message <- page_posts$message %>%
   gsub("[^[:graph:]]", " ", .) %>%
@@ -35,19 +50,26 @@ page_posts$message <- page_posts$message %>%
 
 page_posts$type <- as.factor(page_posts$type)
 
-page_posts$created <- substr(page_posts$created, start = 1,
-                        stop = regexpr("T", page_posts$created) - 1)
+page_posts$created <- substr(page_posts$created,
+                             start = 1,
+                             stop = regexpr("T", page_posts$created) - 1)
 page_posts
 
 ## Get full details of each Facebook Page post
 ## - Iterate through the data frame of Page posts, using 'id' as identifier
 ##   and download the relevant details per post and store in a list on disk.
 rdsfile <- "facebook/nesrea-post-details.rds"
+
 if (!file.exists(rdsfile)) {
   cat("* Harvesting details on Facebook page posts\n")
   post_details <- lapply(page_posts$id, getPost, nesreaToken)
   cat("** Data on individual page posts successfully downloaded\n")
   saveRDS(post_details, file = rdsfile)
   cat("** Details of individual page posts stored in the file",
-      sQuote(basename(rdsfile)), "\n")
+      sQuote(basename(rdsfile)),
+      "\n")
 }
+
+## Store post details in the database
+db <- dbConnect(SQLite(), "data/nesreanigeria.db")
+
