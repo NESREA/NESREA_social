980c28f7aa1964c9e2f981474d56b2d2d649c6a9
BroVic <victorordu@outlook.com>
2017-11-20 17:58:41 +0100

    Add database validation step
    The download will check for the existence of the database file and will create one if absent.

--- download-data.R
+++ download-data.R
@@ -1,5 +1,24 @@
 ## download-data.R
 
+## Ensures pre-existing database file or create one in
+## its absence (candidate for project parameterization)
+validate_db <- function(dBase) {
+  requireNamespace("DBI", quietly = TRUE)
+  requireNamespace("RSQLite", quietly = TRUE)
+  if (!file.exists(dBase)) {
+    cat("* Creating the database\n")
+    con <- dbConnect(SQLite(), dBase)
+    if (dbIsValid(con) && file.exists(dBase)) {
+      cat("** Done\n")
+      dbDisconnect(con)
+    } else {
+      stop("The database could not be created")
+    }
+  }
+}
+
+validate_db("data/nesreanigeria.db")
+
 tw <- "* Twitter"
 fb <- "* Facebook"
 beg <- "downloads started\n"
