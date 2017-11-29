80a6eeb94b89512b04168a1b0f6ddd14fedf787b
Victor Ordu <victorordu@outlook.com>
2017-11-29 21:35:39 +0100

    Properly set working directory

--- data/download-data.R
+++ data/download-data.R
@@ -1,4 +1,8 @@
 ## download-data.R
+
+if (!interactive())  # will start from repo root directory
+  rootDir <- getwd()
+
 setwd(file.path(rootDir, "data"))
 
 ## Ensures pre-existing database file or create one in
