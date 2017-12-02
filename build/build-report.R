59f9dc7cac94ab7471a9b75f1ed2c207a842e8cf
Victor Ordu <victorordu@outlook.com>
2017-12-02 02:00:01 +0100

    Housekeeping written

--- build/build-report.R
+++ build/build-report.R
@@ -1,15 +1,23 @@
 # Knit, save and display report as MS Word document
+
+## Some housekeeping:
+## Set up things as far as directory trees are concerned.
+## This includes making sure we are working in the right place per time.
+## Also make sure we have a folder in place for saving reports.
 repoName <- "NESREA_social"
-if (identical(basename(getwd()), repoName)) {
-  rootDir <- getwd()
+currDir <- getwd()
+if (identical(basename(currDir), repoName)) {
+  rootDir <- currDir
+} else if (!grepl(repoName, currDir, ignore.case = TRUE)) {
+  stop("You are not on the desired file path.")
 } else {
-  currDir <- getwd()
   rootDir <- substr(currDir,
                     start = 0,
                     stop = regexpr(repoName, currDir) + nchar(repoName))
   setwd(rootDir)
-  rm(currDir)
 }
+rm(currDir, repoName)
+
 buildDir <- file.path(rootDir, "build/")
 dataDir <- file.path(rootDir, "data/")
 
@@ -21,6 +29,7 @@ folderlist <- list.dirs(rootDir, recursive = FALSE, full.names = FALSE)
 if (as.character(folder) %in% folderlist)
   dir.create(folder)
 
+## Prompt the user to optionally download fresh data
 if (interactive()) {
   switch(
     menu(choices = c("Yes", "No"),
@@ -30,6 +39,8 @@ if (interactive()) {
   )
 }
 
+## The main job
+cat("* Building the document\n")
 if (!requireNamespace("rmarkdown")) install.packages("rmarkdown")
 rmarkdown::render(
   file.path(buildDir, "report-template.Rmd"),
@@ -38,6 +49,7 @@ rmarkdown::render(
   output_dir = folder
 )
 
-system(paste("open", file.path(folder, filename)))
-
+try(
+  system(paste("open", file.path(folder, filename)))
+)
 rm(filename, folder, folderlist, rootDir, buildDir)
