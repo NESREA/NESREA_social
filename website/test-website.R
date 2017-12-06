92c9d6088196d849c2213364fba318c0832898d9
Victor Ordu <victorordu@outlook.com>
2017-12-06 17:42:00 +0100

    Initial commit

---
+++ website/test-website.R
@@ -0,0 +1,20 @@
+## test-website.R
+suppressPackageStartupMessages(library(rvest, quietly = TRUE))
+
+context("Website related functions")
+
+test_that("Test site is appropriate", {
+  expect_that(inherits(exampleDotCom, "xml_document"), is_true())
+  expect_that(inherits(exampleDotCom, "xml_node"), is_true())
+})
+
+test_that("Data is scraped from the website", {
+  expect_equal(scrape_items(exampleDotCom, "h1"), "Example Domain")
+  expect_equal(typeof(scrape_items(exampleDotCom, "p")), "character")
+  expect_that(
+    gregexpr("domain", scrape_items(exampleDotCom, "p"))[[1]][1] == 6,
+    is_true())
+  expect_that(
+    gregexpr("domain", scrape_items(exampleDotCom, "p"))[[1]][2] == 100,
+    is_true())
+})
