ca6df288099f7a7c25ec20b12b9ae607a513b87e
Victor Ordu <victorordu@outlook.com>
2018-06-11 17:08:39 +0100

    Add conditional to Batch file
    Enable the exclusive updating of the database on one hand and the building
    of the document on the other. This can only be done via the command line.
    The arguments to be passed to the script are '--update' and '--build',
    respectively.

--- nsoc.cmd
+++ nsoc.cmd
@@ -1,15 +1,15 @@
 :: nsoc.cmd
 ::
-:: Batch file for running 'NESREA_social' project
+:: Batch file for starting the 'NESREA_social' project
 @ECHO OFF
 
 SETLOCAL ENABLEEXTENSIONS
-
-SET me=%~n0
+SET this=%~n0
 SET parent=%~dp0
+
+SET arg=%1
 SET exec=Rscript.exe
 SET rflags=--vanilla
-SET upd=--update
 
 :: Error codes
 SET /A errno=0
@@ -18,13 +18,15 @@ SET /A ERROR_BUILD_FAILED=2
 
 
 :: Update the database
-IF "%upd%"=="--update" (
-	%exec% %rflags% src/download.R
+IF "%arg%" NEQ "--build" (
+    %exec% %rflags% src/download.R
 )
 
 :: Build the main report
-%exec% %rflags% src/build.R
+IF "%arg%" NEQ "--update" (
+    %exec% %rflags% src/build.R
+)
 
 IF %ERRORLEVEL% NEQ 0 (
-    ECHO %me%: %exec% returned with code %ERRORLEVEL%
+    ECHO %this%: %exec% returned with code %ERRORLEVEL%
 )
